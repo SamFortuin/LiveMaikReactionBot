7320e9992b110bd6223f158481e064345d74307e
SamFortuin <samuel.fortuin@outlook.com>
2023-01-08 21:28:06 +0100

    added comments and removed some test junk

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# bot.py
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -11,39 +10,23 @@ intents.message_content = True
 intents.guilds = True
 intents.voice_states = True
 
-f = open('TOKEN.txt','r')
+f = open('TOKEN.txt','r') #grabs token from local file
 TOKEN = f.read()
 f.close()
 
-bot = commands.Bot(command_prefix='//', intents=intents)
+bot = commands.Bot(command_prefix='//', intents=intents) #discord.ext way of creating user
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-
-# @bot.command()
-# async def hello(ctx):
-#     if ctx.author != bot.user:
-#         await ctx.send('Heya')
-
-# @bot.command()
-# async def repeat(ctx,arg):
-#     await ctx.send(arg)
-        
-# @bot.command()
-# async def listMember(ctx, arg: int):
-#     a = ctx.guild.members
-#     await ctx.send(a[arg])
+    print(f'{bot.user} is ready')
 
 @bot.event
 async def on_voice_state_update(member,before,after):
     if str(member) == "Sammie#0901": #[ ]change to lothym#5083
-        # a = str(after).split(sep=" ")
-        # for x in a:
-        #     print(x)
-        # print("\n\n")
-        a = discord.VoiceState.self_video()
-        print(a)
+        a = str(after).split(sep=" ") # grabs after object and turns it into a list for reading
+        for x in a: #I forgor how to do it with *a and sep so this is how we're doing it for now
+            print(x)
+        print("\n\n")
 
 
 bot.run(TOKEN)
