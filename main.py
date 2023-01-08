5d0bae21a7192ee1a34ee1cbefff5416a9cd330d
SamFortuin <samuel.fortuin@outlook.com>
2023-01-08 21:32:26 +0100

    improved read method

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.guilds = True
 intents.members = True
 intents.messages = True
-intents.typing = True
 intents.message_content = True
-intents.guilds = True
 intents.voice_states = True
 
-f = open('TOKEN.txt','r') #grabs token from local file
-TOKEN = f.read()
-f.close()
+#grabs token from local file
+with open('TOKEN.txt','r') as f:
+    TOKEN = f.read()
 
 bot = commands.Bot(command_prefix='//', intents=intents) #discord.ext way of creating user
 
@@ -24,7 +22,7 @@ async def on_ready():
 async def on_voice_state_update(member,before,after):
     if str(member) == "Sammie#0901": #[ ]change to lothym#5083
         a = str(after).split(sep=" ") # grabs after object and turns it into a list for reading
-        for x in a: #I forgor how to do it with *a and sep so this is how we're doing it for now
+        for x in a: #I forgor how to do it with *a and sep so this is how we're doing it for now, we won't need this shit in the future anyway
             print(x)
         print("\n\n")
 
